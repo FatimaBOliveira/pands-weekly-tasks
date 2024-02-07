0afe68fd8a2b01014aae317a806c265c97545d43
Fatima Oliveira <Fatima.21.00@hotmail.com>
2024-02-07 11:28:16 +0000

    week 2

--- bank.py
+++ bank.py
@@ -1,15 +1,15 @@
-#Bank
-#Bank tranfer
-#Author: Fatima
+# Bank
+# Bank transaction
+# Author: Fatima
 
-#a = 65 (in cent)
-#b = 180 (in cent)
-#https://realpython.com/lessons/floating-point-numbers-python/
+# a = 65 (in cent)
+# b = 180 (in cent)
+# https://realpython.com/lessons/floating-point-numbers-python/
+# https://docs.python.org/3/library/functions.html#chr
 
 a= float(.65) 
 b= float(1.80)
 
-
 sum = (a + b)
 
-print(f" The sum of these is: €{sum}. ")
+print(f" The sum of these is: {chr(8364)}{sum}.")
