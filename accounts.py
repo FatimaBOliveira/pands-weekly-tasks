c1b7c4f62d61295d85ce8a2f7fd06cd2a5274adf
Fatima Oliveira <Fatima.21.00@hotmail.com>
2024-02-11 15:36:18 +0000

    week 3

--- accounts.py
+++ accounts.py
@@ -2,11 +2,20 @@
 # Program that shows only the last 4 digits of the bank account number
 # Author: Fatima
 
-
-#Extra:Modify the program to deal with account numbers of any length (yes that is a vague requirement, comment your assumptions)
-
 accountnumber = "1234567890"
 
 print ((f"Please enter an 10 digit account number: ") + str(accountnumber))
 
-print (accountnumber [6:])
+#https://www.w3schools.com/python/python_strings_modify.asp
+
+print (accountnumber.replace("123456", "XXXXXX"))
+
+
+#Extra:Modify the program to deal with account numbers of any length
+#   To use present any bank number with only the last 4 digits showing, I will request the client to write the first numbers and the last 4 numbers apart, so I can use the "replace" funcion for the final presentation of the number.
+
+accountnumberA = str(int(input("Please enter the first numbers of your account: ")))
+a = (accountnumberA.replace(accountnumberA, "XXXXXX"))
+accountnumberB = int(input("Please enter the last 4 numbers of your account: "))
+
+print (f"{a}{accountnumberB}")
