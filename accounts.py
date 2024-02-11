8c9bb6da40ed8ddda184099273689c639a4cf6b5
Fatima Oliveira <Fatima.21.00@hotmail.com>
2024-02-11 14:16:16 +0000

    3 week

---
+++ accounts.py
@@ -0,0 +1,12 @@
+# Accounts
+# Program that shows only the last 4 digits of the bank account number
+# Author: Fatima
+
+
+#Extra:Modify the program to deal with account numbers of any length (yes that is a vague requirement, comment your assumptions)
+
+accountnumber = "1234567890"
+
+print ((f"Please enter an 10 digit account number: ") + str(accountnumber))
+
+print (accountnumber [6:])
