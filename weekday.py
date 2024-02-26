6380ef965c344a7f3dc7c0cb5b2f67104f45db59
Fatima Oliveira <Fatima.21.00@hotmail.com>
2024-02-26 17:29:14 +0000

    Week 5

--- weekday.py
+++ weekday.py
@@ -1,5 +1,5 @@
 # Weekday
-# Program that outputs if today is a weekday or a day in weekend 
+# Program that outputs if today is a weekday or a day in weekend.
 #Author: Fatima Oliveira
 
 from datetime import date  # Will export today's time # https://realpython.com/python-datetime/
@@ -7,8 +7,16 @@ from datetime import date  # Will export today's time # https://realpython.com/p
 
 today = date.today() # https://docs.python.org/3/library/datetime.html#datetime.date.today
 
-if today.weekday() == [0, 4]:  #https://docs.python.org/3/library/datetime.html#datetime.date.weekday
+if today.weekday() == 0:  #https://docs.python.org/3/library/datetime.html#datetime.date.weekday
     # 0 is correspondent to Monday, then 1 to Tuesday, then 2 to Wednesday, 3 to Thursday and 4 to Friday
+    print("Yes, unfortunately today is a weekday.")
+elif today.weekday() == 1:
+    print("Yes, unfortunately today is a weekday.")
+elif today.weekday() == 2:
+    print("Yes, unfortunately today is a weekday.")
+elif today.weekday() == 3:
+    print("Yes, unfortunately today is a weekday.")
+elif today.weekday() == 4:
     print("Yes, unfortunately today is a weekday.")
 else: # Values above 4 are weekend days
     print("It is the weekend, yay!")
