cb908f1a45acbe7b0d67ad0f3dd7e8a7cce53fdb
Fatima Oliveira <Fatima.21.00@hotmail.com>
2024-03-10 20:20:15 +0000

    updated

--- bank.py
+++ bank.py
@@ -1,15 +1,10 @@
 # Bank
-# Bank transaction
-# Author: Fatima
+# Bank operation that sums two amounts in cents and outputs the sum transaction in euros.
+# Author: Fatima Oliveira
 
-# a = 65 (in cent)
-# b = 180 (in cent)
-# https://realpython.com/lessons/floating-point-numbers-python/
+a = (int(input("Enter amount1(in cent): ")))
+b = (int(input("Enter amount2(in cent): ")))
+sum = ((a + b) / 100) # 1 euro is equivalent to 100 cent, so if we divide the sum by 100, we will get the sum in euros.
+print(f" The sum of these is: {chr(8364)}{sum}.") 
+# To print the euro sign without using the actual sign we can use the chr() function.
 # https://docs.python.org/3/library/functions.html#chr
-
-a= float(.65) 
-b= float(1.80)
-
-sum = (a + b)
-
-print(f" The sum of these is: {chr(8364)}{sum}.")
